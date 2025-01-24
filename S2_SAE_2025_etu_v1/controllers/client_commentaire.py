a8b8ea40ff895bb19dd91b6ba729581e914469f2
Mathias Dumain <mathias.dumain@edu.univ-fcomte.fr>
2025-01-24 10:04:33 +0100

    show article et type_article

--- S2_SAE_2025_etu_v1/controllers/client_commentaire.py
+++ S2_SAE_2025_etu_v1/controllers/client_commentaire.py
@@ -14,17 +14,17 @@ client_commentaire = Blueprint('client_commentaire', __name__,
 @client_commentaire.route('/client/article/details', methods=['GET'])
 def client_article_details():
     mycursor = get_db().cursor()
-    id_article =  request.args.get('id_article', None)
+    id_article =  request.args.get('id_parfum', None)
     id_client = session['id_user']
 
     ## partie 4
     # client_historique_add(id_article, id_client)
 
     sql = '''
+    SELECT * FROM parfum WHERE id_parfum = %s
     '''
-    #mycursor.execute(sql, id_article)
-    #article = mycursor.fetchone()
-    article=[]
+    mycursor.execute(sql, id_article)
+    article = mycursor.fetchone()
     commandes_articles=[]
     nb_commentaires=[]
     if article is None:
