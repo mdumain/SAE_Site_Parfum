df122ebb4347291c4f25c6b9a6ea1d0e12982b4d
Leenknegt Noa <noa.leenknegt@edu.univ-fcomte.fr>
2025-02-13 14:32:23 +0100

    a

--- S2_SAE_2025_etu_v1/connexion_db.py
+++ S2_SAE_2025_etu_v1/connexion_db.py
@@ -10,10 +10,11 @@ load_dotenv(os.path.join(project_folder, '.env'))
 def get_db():
     if 'db' not in g:
         g.db =  pymysql.connect(
-            host=os.environ.get("HOST"),
-            user=os.environ.get("LOGIN"),
-            password=os.environ.get("PASSWORD"),
-            database=os.environ.get("DATABASE"),
+            host="localhost",
+            user="admin",
+            port=3306,
+            password="mdp1",
+            database="sae",
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
